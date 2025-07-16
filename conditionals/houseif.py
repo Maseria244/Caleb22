0af9b2ca5adab1fd3bb3119089b7f99ef6d72d4b
Maseria244 <146533991+Maseria244@users.noreply.github.com>
2025-07-16 07:33:38 +0300

    IF STATEMENTS AND WHILE LOOPS
    .

--- conditionals/houseif.py
+++ conditionals/houseif.py
@@ -3,13 +3,13 @@ good_credit = False
 
 if good_credit:
     pd = 10/100 * price
-    print(f"Put down ${pd}")
+    print(f"Put down ${pd:,.2f}")
 
 else:
     pd = 20/100 * price
-    print(f"Put down ${pd}")
+    print(f"Put down ${pd:,.2f}")
 
-print(f"Your down payment is ${pd} ")
+print(f"Your down payment is ${pd:,.2f} ")
 
 
 
--- conditionals/while.py
+++ conditionals/while.py
@@ -5,9 +5,108 @@ while i<=5:
 
 print("Gone")'''
 
-i = 3
+'''i = 3
 while i <= 10:
     print('*' * i)
     i = i + 1
 
-print ("Done")
+print ("Done")'''
+
+'''name = input("Enter your name ")
+
+while name == "":
+    print("You did not enter anything")
+    name = input("Enter your name ")
+else:
+    print(f"Hello {name}")'''
+
+'''i = 1
+
+while i <= 6:
+    print('r' * i)
+    i += 1'''
+
+'''age = int(input("Enter your age: "))
+
+while age <= 0:
+    print("Your age can't be a non-positive number")
+    age = int(input("Enter your age: "))
+else:
+    print(f"Hey you are {age} years old'''
+
+'''food = input("Enter food you like (q to quit)")
+
+while  food != 'q':
+    print(f"You like {food}.")
+    food = input("Enter another food you like (q to quit)")
+
+print("You eat alot")'''
+
+'''num = int(input("Enter a num 1 -10:"))
+
+while num <= 0 or num > 10:
+    print("Read instructions!!")
+    num = int(input("Enter a num 1 -10"))
+
+print(f"You chose {num}")'''
+
+'''command = input("")
+
+if command == 'help':
+    print("Start --> Start the car")
+    print("Stop --> Stop the car")
+    print("quit --> quit the game")
+
+while command == 'start':
+    print("Car started!")
+    command = input("")
+    while command == 'stop':
+        print("Car stopped!") 
+        command = input("")
+        while command == 'quit':
+            print("Bye")
+            break
+else:
+    print("Not recognised as an internal or external command")'''
+
+'''command = input("")
+
+while command == 'help':
+    print("Start --> Start the car")
+    print("Stop --> Stop the car")
+    print("quit --> quit the game")
+    command = input("")
+    while command == 'start':
+        print("Car started!")
+        command = input("")
+        while command == 'stop':
+            print("Car stopped!") 
+            command = input("")
+            while command == 'quit':
+                break
+
+
+
+
+
+print("Not recognised as an internal or external command")'''
+
+command = ""
+while True:
+    command = input("> ").lower()
+    if command == 'start':
+        print("Car started")
+    elif command == 'stop':
+        print("Car stop")
+    elif command == 'help':
+        print("Start --> Start the car")
+        print("Stop --> Stop the car")
+        print("quit --> quit the game")
+    elif command == 'quit':
+        break
+    else:
+        print("Not recognised as an internal or external command")
+
+
+
+
